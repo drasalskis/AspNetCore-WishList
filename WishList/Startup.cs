5c3076a0a5325f84567c083a6c454ab8d17ebaef
DoMuX <domuxas@gmail.com>
2020-11-13 23:50:09 +0200

    Completed module

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -28,7 +28,7 @@ namespace WishList
             else app.UseExceptionHandler("/Home/Error");
 
             app.UseRouting();
-            app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
         }
     }
 }
